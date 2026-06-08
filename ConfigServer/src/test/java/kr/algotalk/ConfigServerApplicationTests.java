dbad3cb4a4fb0fa6f15b809bb801451a64a46345
AnJungHoon <jh96.dev@gmail.com>
2026-06-08 16:53:29 +0900

    feat: 배포 환경 Config 설정 추가
    - USER-SERVICE-prod.yml 배포 환경 설정
    - API-GATEWAY-prod.yml 배포 환경 설정
    - COMMUNITY-SERVICE-prod.yml 배포 환경 설정
    - INTERVIEW-SERVICE-prod.yml 배포 환경 설정

---
+++ ConfigServer/src/test/java/kr/algotalk/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.algotalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
